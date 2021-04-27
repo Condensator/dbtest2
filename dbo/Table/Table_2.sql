70e0705c5cdf118d8a7f947d8a44bba8c25316bd
Condensator <7a9f3f719c69e633f73f9a959d62ff7f2675741d>
2021-04-27 17:22:00 +0300

    init commit

---
+++ dbo/Table/Table_2.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[Table_2] (
+    [zxcv] NCHAR (10) NULL,
+    [qwe]  NCHAR (10) NULL
+);
+
+
+GO
+
---
+++ dbo/Table/Table_3.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[Table_3] (
+    [qwer] NCHAR (10) NULL
+);
+
+
+GO
+
